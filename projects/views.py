31e4db4a3d6775ae4ae596518879af42c6fb255d
ariyan <ariyanbolandi3@gmail.com>
2025-07-22 10:08:14 +0330

    tweak: add user-customized tags functionality

--- projects/views.py
+++ projects/views.py
@@ -58,9 +58,14 @@ def update_project(request, slug):
     project = profile.project_set.get(slug=slug)
     form = ProjectForm(instance=project)
     if request.method == "POST":
+        newtags = request.POST.get("newtags").replace(",", " ").split()
+
         form = ProjectForm(request.POST, request.FILES, instance=project)
         if form.is_valid():
-            form.save()
+            project = form.save()
+            for tag in newtags:
+                tag, created = Tag.objects.get_or_create(name=tag)
+                project.tags.add(tag)
             return redirect("account")
 
     context = {"form": form}
